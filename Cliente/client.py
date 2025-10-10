2835e53e610c6eac27a38a8d49ae36b023030b73
GNS03 <gabrielnogueirasouza@gmail.com>
2025-10-10 15:29:44 -0300

    update client
    Add menu and change from using Aluno to AlunoController, tested aluno: inserir, editar, pesquisar, remover

--- Cliente/client.py
+++ Cliente/client.py
@@ -6,6 +6,88 @@ import Pyro5.socketutil
 
 from model import Aluno
 
+
+def menu():
+
+    opcao = input(
+    """
+    1 - Alunos
+    2 - Disciplinas
+    3 - Avaliacoes
+    4 - Frequencia
+    0 - Sair
+    """
+    )
+
+    match(opcao):
+        case 1:
+            classe = "AlunoController"
+
+            uri = f"PYRO:{classe}@{my_ip}:{port}"
+
+            aluno_control = Pyro5.api.Proxy(uri)
+
+            opcao = input(
+            """
+            1 - Inserir
+            2 - Editar
+            3 - Pesquisar
+            4 - Deletar
+            0 - Sair
+            """
+            )
+
+            match(opcao):
+                case 1:
+                    matricula = int(input("Matricula: "))
+                    nome = input("Nome: ")
+                    idade = int(input("Idade: "))
+                    periodo = int(input("Periodo: "))
+
+                    if aluno_control.inserir(matricula, nome, idade, periodo):
+                        print("SQL executed successfully.")
+                    else:
+                        print("An error ocurred check the server for details")
+                case 2:
+                    matricula = int(input("Matricula: "))
+
+                    alunos = aluno_control.pesquisar(matricula)
+                    if alunos:
+                        print(alunos)
+                    else:
+                        print("Did not find that key")
+                case 3:
+                    print("Aperte enter para nao modificar o campo")
+                    matricula = int(input("Matricula: "))
+                    nome = input("Nome: ")
+                    idade = int(input("Idade: "))
+                    periodo = int(input("Periodo: "))
+
+                    if aluno_control.editar(matricula, nome, idade, periodo):
+                        print("SQL executed successfully.")
+                    else:
+                        print("An error ocurred check the server for details")
+                case 4:
+                    matricula = int(input("Matricula: "))
+
+                    alunos = aluno_control.remover(matricula)
+                    if alunos:
+                        print(alunos)
+                    else:
+                        print("Did not find that key")
+                case 0:
+                    raise NotImplementedError
+
+        case 2:
+            raise NotImplementedError
+        case 3:
+            raise NotImplementedError
+        case 4:
+            raise NotImplementedError
+        case 0:
+            raise NotImplementedError
+
+
 if __name__ == "__main__":
 
     # Utils
@@ -17,10 +99,20 @@ if __name__ == "__main__":
     # nameserver = Pyro5.api.locate_ns(host=my_ip, port=9090)
     # print("Nameserver found")
 
-    classe = "Aluno"
+    classe = "AlunoController"
 
     uri = f"PYRO:{classe}@{my_ip}:{port}"
 
-    aluno = Pyro5.api.Proxy(uri)
+    aluno_control = Pyro5.api.Proxy(uri)
 
-    aluno.hello()
+    # if aluno_control.inserir(3, "Eduardo", 24, 4):
+    #     print("SQL executed successfully.")
+    # else:
+    #     print("An error ocurred check the server for details")
+
+    # alunos = aluno_control.pesquisar(2)
+    # print(alunos)
+
+    # aluno_control.editar(1, "Fer")
+
+    # aluno_control.remover(2)
---
+++ Servidor/model/aluno_util/aluno_controller.py
@@ -0,0 +1,75 @@
+from Servidor.model import Aluno, clear_screen
+from Servidor.model.aluno_util.aluno_interface import AlunoInterface
+from Servidor.model.sql_connect.sql import sql_execute
+
+
+class AlunoController(AlunoInterface):
+
+    def __init__(self):
+        pass
+
+    def inserir(self, matricula: int, nome: str, idade: int, periodo: int) -> bool:
+        op = "Insert"
+
+        aluno = Aluno(matricula, nome, idade, periodo)
+
+        params = {"matricula": aluno.matricula, "nome": aluno.nome, "idade": aluno.idade, "periodo": aluno.periodo}
+
+        sql_str = "INSERT INTO aluno (matricula, nome, idade, periodo) VALUES (%(matricula)s, %(nome)s, %(idade)s, %(periodo)s)"
+        sql_execute(op, sql_str, params, fetch=False)
+
+        return True
+
+    # Nao e serializavel, ent tem q usar dict
+    def pesquisar(self, matricula: int) -> None | list[dict]:
+        op = "Search"
+
+        # TODO: pesquisar por nome?
+
+        params = {"matricula": matricula}
+
+        sql_str = "SELECT matricula, nome, idade, periodo FROM aluno WHERE matricula = %(matricula)s"
+        alunos = sql_execute(op, sql_str, params, fetch=True)
+
+        return alunos
+
+    def editar(self, matricula: int, nome: str = None, idade: int = None, periodo: int = None) -> \
+            bool:
+        op = "Edit"
+
+        novo = {"matricula": matricula, "nome": nome, "idade": idade, "periodo": periodo}
+
+        # Pega os valores atuais do aluno
+        print(f"Pegando valores da matricula: {matricula}")
+        aluno = self.pesquisar(matricula)[0]
+
+        # Se os valores passados para a funcao forem diferentes de None, substitui o valor atual
+        for key, value in aluno.items():
+            if novo.get(key) is not None:
+                aluno[key] = novo.get(key)
+
+        params = aluno
+
+        sql_str = ("UPDATE aluno SET nome = %(nome)s, idade = %(idade)s, periodo = %(periodo)s "
+                   "WHERE "
+                   "matricula "
+                   "= %(matricula)s")
+        sql_execute(op, sql_str, params, fetch=False)
+
+        return True
+
+    def remover(self, matricula: int) -> bool:
+        op = "Delete"
+
+        aluno = Aluno(matricula)
+
+        params = {"matricula": aluno.matricula}
+
+        sql_str = "DELETE FROM aluno WHERE matricula = %(matricula)s"
+        sql_execute(op, sql_str, params, fetch=False)
+
+        return True
+
+
+
+
