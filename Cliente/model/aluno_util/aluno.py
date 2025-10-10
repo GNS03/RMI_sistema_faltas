c544624e8ed6168a5d46d8e8345be2a98cc3da6f
GNS03 <gabrielnogueirasouza@gmail.com>
2025-10-10 15:35:04 -0300

    Update aluno.py

--- Cliente/model/aluno_util/aluno.py
+++ Cliente/model/aluno_util/aluno.py
@@ -1,10 +1,15 @@
 class Aluno:
 
-    def __init__(self, matricula: int = 0, nome: str = "", idade: int = 0):
+    def __init__(self, matricula: int = 0, nome: str = "", idade: int = 0, periodo: int = 0, disciplinas_matriculadas=None):
+
+        if disciplinas_matriculadas is None:
+            disciplinas_matriculadas = []
 
         self.matricula = matricula
         self.nome = nome
         self.idade = idade
+        self.periodo = periodo
+        self.disciplinas_matriculadas: list[int] = disciplinas_matriculadas
 
     def hello(self):
-        print("Hello")
+        print(f"Hello {self.nome}")
