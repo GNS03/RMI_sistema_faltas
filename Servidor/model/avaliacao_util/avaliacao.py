ca37ee8aacc15d76b8943afebaa9276fdf62daa8
GNS03 <gabrielnogueirasouza@gmail.com>
2025-10-13 20:23:42 -0300

    add __init__ e to_dict to avaliacao.py

--- Servidor/model/avaliacao_util/avaliacao.py
+++ Servidor/model/avaliacao_util/avaliacao.py
@@ -1,3 +1,13 @@
 class Avaliacao:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, id: int, disciplina_k: int, aluno_k: int):
+
+        self.id = id
+        self.disciplina_k = disciplina_k
+        self.aluno_k = aluno_k
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "disciplina_k": self.disciplina_k,
+            "aluno_k": self.aluno_k,
+        }
