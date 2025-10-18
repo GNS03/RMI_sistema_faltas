2905ddf1ac81c441a7deb79176db50e2ec450425
GNS03 <gabrielnogueirasouza@gmail.com>
2025-10-18 19:26:38 -0300

    Update __init__.py

--- Servidor/model/__init__.py
+++ Servidor/model/__init__.py
@@ -1,4 +1,4 @@
 from Servidor.model.aluno_util.aluno import *
 from Servidor.model.avaliacao_util.avaliacao import *
 from Servidor.model.disciplina_util.disciplina import *
-from Servidor.model.frequencia_util.frequencia import *
+from Servidor.model.professor_util.professor import *
