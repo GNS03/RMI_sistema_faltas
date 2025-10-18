fd730c9a42d9efe1891ab4bafc3bc12206dd230c
GNS03 <gabrielnogueirasouza@gmail.com>
2025-10-18 19:20:01 -0300

    Update avaliacao.py

--- Cliente/model/avaliacao_util/avaliacao.py
+++ Cliente/model/avaliacao_util/avaliacao.py
@@ -1,3 +1,15 @@
 class Avaliacao:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, id: int, disciplina_fk: int, aluno_fk: int, nota: float):
+
+        self.id = id
+        self.disciplina_fk = disciplina_fk
+        self.aluno_k = aluno_fk
+        self.nota = nota
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "disciplina_fk": self.disciplina_fk,
+            "aluno_fk": self.aluno_fk,
+            "nota": self.nota
+        }
